6b90e819f038ce3936045e410c364ba1a74ac7d2
aydy315 <vallejosllantoya@gmail.com>
2026-07-20 02:25:03 -0500

    Add files via upload

--- Operaciones.java
+++ Operaciones.java
@@ -1,7 +1,12 @@
-package c05.p04;
+package c05.p05;
 
-public class Operaciones {
-    public void imprimirTexto(String texto) {
-        System.out.println(texto);
-    } // Fin del método imprimirTexto
+class Operaciones {
+    public void concatenarTexto(String texto1, String texto2) {
+        System.out.println(texto1 + " " + texto2);
+    } // Fin del método concatenarTexto
+
+    public void imprimirSuma(int entero1, int entero2) {
+        int suma = entero1 + entero2;
+        System.out.println("Suma: " + suma);
+    } // Fin del método imprimirSuma
 } // Fin de la clase Operaciones
